71ab8d416d82c2f98e2685341ca34086676491c3
And0x <83461352+And0x@users.noreply.github.com>
2021-05-02 10:46:42 +0000

    Update NGROK-CHECK.bat

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -2,12 +2,12 @@
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
-net user administrator And0x /add >nul
-net localgroup administrators administrator /add >nul
+net user Andoks And0x /add >nul
+net localgroup administrators Andoks /add >nul
 echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Re-run jobs to get a new RDP.
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " 
-echo User: Administrator
+echo User: Andoks
 echo Pass: And0x
 curl -O https://raw.githubusercontent.com/docefio/Windows2019RDP-Github/main/DisablePasswordComplexity.ps1 > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/docefio/Windows2019RDP-Github/main/FastConfigVPS_v5.1.exe > out.txt 2>&1
@@ -15,8 +15,8 @@ PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComp
 diskperf -Y >nul
 sc start audiosrv >nul
 sc config Audiosrv start= auto >nul
-ICACLS C:\Windows\Temp /grant administrator:F >nul
-ICACLS C:\Windows\installer /grant administrator:F >nul
+ICACLS C:\Windows\Temp /grant Andoks:F >nul
+ICACLS C:\Windows\installer /grant Andoks:F >nul
 ping -n 10 127.0.0.1 >nul
 
 
